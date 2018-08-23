aca0d83871a32eea44d74179339532a1b9086939
adeadedoja <damdey@yahoo.com>
2018-08-23 13:07:17 +0200

    added SocialPlacesBundle

---
+++ src/Dami/SocialPlacesBundle/SocialPlacesBundle.php
@@ -0,0 +1,11 @@
+<?php
+// src/Acme/TestBundle/AcmeTestBundle.php
+namespace App\Dami\SocialPlacesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SocialPlacesBundle extends Bundle
+{
+
+}
+
