c1a34c1b5c4ce3224527a338dd3f17bd87d79756
adeadedoja <damdey@yahoo.com>
2018-08-23 13:31:06 +0200

    added contact_store route

--- src/Dami/SocialPlacesBundle/SocialPlacesBundle.php
+++ src/Dami/SocialPlacesBundle/SocialPlacesBundle.php
@@ -3,9 +3,16 @@
 namespace App\Dami\SocialPlacesBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpFoundation\Response;
 
 class SocialPlacesBundle extends Bundle
 {
+    public function store(){
+        $number = random_int(0, 100);
 
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
 }
 
