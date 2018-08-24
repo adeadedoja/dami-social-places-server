7ea00112bda7230c5c336f253d7dc08f6a568a38
adeadedoja <damdey@yahoo.com>
2018-08-25 00:52:43 +0200

    Form successfulty saving into database

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request)
     {
+        //new contact object
+        $contact = new Contact;
+
+        $form = $this->createFormBuilder($contact)
+            ->add('name', TextType::class)
+            ->add('phone', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('subject', TextType::class)
+            ->add('message', TextareaType::class)
+            ->add('save', SubmitType::class, array('label' => 'Get in Touch'))
+            ->getForm();
+        
+        $form->handleRequest($request);
+
+        //checks if submitted and valid
+        if ($form->isSubmitted() && $form->isValid()) {
+            //gets the submitted value and assigns it to the contact object
+            $contact = $form->getData();
+
+            //save to DB
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('contact_add_success');
+        }
+        
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'title' => 'Social Places Contact',
+             'form' => $form->createView(),
         ]);
     }
 }
